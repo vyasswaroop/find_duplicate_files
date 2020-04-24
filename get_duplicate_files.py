7eae4b85c3b76f48a5c043e164542d5e4b825760
vyasswaroop <vyasswaroop.mec@gmail.com>
2020-04-24 21:35:40 +0530

    Directory path as argument

--- get_duplicate_files.py
+++ get_duplicate_files.py
@@ -1,4 +1,9 @@
-import hashlib, os
+import hashlib, os, argparse
+
+def getAbsolutePath(fileName, fileDir):
+    if fileDir != ".":
+        return fileDir + "/" + fileName
+    return fileName
 
 def readByChunk(fileObj, chunkSize=1024):
     while True:
@@ -13,10 +18,10 @@ def hashFileByChunk(fileObj):
         hashChunk.append(hashlib.md5(chunk).hexdigest())
     return tuple(hashChunk)
 
-def groupBymd5(group):
+def groupBymd5(group, fileDir):
     unique = {}
     for filename in group:
-        with open(filename, 'rb') as fileObj:
+        with open(getAbsolutePath(filename, fileDir), 'rb') as fileObj:
             filehash = hashFileByChunk(fileObj)
         if not filehash in unique:
             unique[filehash] = [filename]
@@ -24,24 +29,30 @@ def groupBymd5(group):
             unique[filehash].append(filename)
     return list(unique.values())
 
-def groupBySize(fileList):
+def groupBySize(fileDir, fileList):
     unique = {}
     for filename in fileList:
-        filesize = os.path.getsize(filename)
+        filesize = os.path.getsize(getAbsolutePath(filename, fileDir))
         if not unique.get(filesize):
             unique[filesize] = [filename]
         else:
             unique[filesize].append(filename)
     return list(unique.values())
 
-def displayFiles(sortedFiles):
+def displayDuplicates(sortedFiles):
     for filesList in sortedFiles:
         if len(filesList) != 1 :
             print(" ".join(filesList))
 
 if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Find duplicate files.')
+    parser.add_argument(
+        'path', metavar='N',
+        help='an integer for the accumulator'
+    )
+    args = parser.parse_args()
     hashSorted = []
-    fileList = [filename for filename in os.listdir('.') if os.path.isfile(filename)]
-    sizeSorted = groupBySize(fileList)
+    fileList = [filename for filename in os.listdir(args.path) if os.path.isfile(args.path + "/" +filename)]
+    sizeSorted = groupBySize(args.path, fileList)
     for group in sizeSorted:
-        displayFiles(groupBymd5(group))
+        displayDuplicates(groupBymd5(group, args.path))
