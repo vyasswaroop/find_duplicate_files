c7befe6d5ebfe7ca6fca4b92eb179dc97b8860a5
vyasswaroop <vyasswaroop.mec@gmail.com>
2020-04-24 20:05:24 +0530

    For larger files, first sort with size and then compare with chunk

--- get_duplicate_files.py
+++ get_duplicate_files.py
@@ -1,11 +1,34 @@
 import hashlib, os
 
-unique = dict()
-for filename in os.listdir('.'):
-    if os.path.isfile(filename):
+def getChunk(file, chunkSize=1024):
+    with open(file, 'rb') as fileObj:
+        chunk = fileObj.read(chunkSize)
+        return hashlib.md5(chunk).hexdigest()
+
+def groupBymd5(group):
+    unique = {}
+    for filename in group:
+        filehash = getChunk(filename)
         filehash = hashlib.md5(open(filename, 'rb').read()).hexdigest()
         if not unique.get(filehash):
             unique[filehash] = [filename]
         else:
             unique[filehash].append(filename)
-print(list(unique.values()))
+    print(list(unique.values()))
+    return unique
+
+def groupBySize(fileList):
+    unique = {}
+    for filename in fileList:
+        filesize = os.path.getsize(filename)
+        if not unique.get(filesize):
+            unique[filesize] = [filename]
+        else:
+            unique[filesize].append(filename)
+    return list(unique.values())
+
+if __name__=='__main__':
+    fileList = [filename for filename in os.listdir('.') if os.path.isfile(filename)]
+    groupList = groupBySize(fileList)
+    for group in groupList:
+        groupBymd5(group)
